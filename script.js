02198bbdb4349ef516d8ae07e27d9d9cbe4e2680
Mohit Jha <95844984+mohitanand744@users.noreply.github.com>
2024-04-28 15:46:18 +0530

    Solved few responsiveness problems

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let apiUrl =
   "https://v6.exchangerate-api.com/v6/9938a6ae2b4f18570f810514/latest/USD";
+let fromCurrency = document.querySelector(".fromcurrency");
+let toCurrency = document.querySelector(".tocurrency");
 
+currencies.forEach((Currency) => {
+  let options = document.createElement("option");
+  options.value = Currency;
+  options.text = Currency;
+  fromCurrency.append(options);
+});
 
-  currencies.forEach((c) => {
-  /*   console.log(c); */
-  })
+currencies.forEach((Currency) => {
+  let options = document.createElement("option");
+  options.value = Currency;
+  options.text = Currency;
+  toCurrency.append(options);
+});
